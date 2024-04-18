971ff01f03df131eec1467a12e03cc24b28bdebf
vitor <vitor@LDI301>
2024-04-18 11:57:48 -0300

    Add project files.

---
+++ POO-Aula1_2/Aluno.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Aula1_2
+{
+    public class Aluno
+    {
+        private string nome;
+        public double nota;
+        //ATRIBUTOS - características
+
+        public string? Nome
+        {
+            get => this.nome.ToUpper();
+            set
+            {
+                if (value != null && value != "")
+                    this.nome = value;
+            }
+        }
+        //PROPRIEDADES - validações
+
+        public Aluno(string nome)
+        {
+            this.nome = nome;
+        }
+        public Aluno(string nome, double nota)
+        {
+            this.nome = nome;
+            this.nota = nota;
+        }
+        public Aluno() { }
+        //Construtor
+        public bool Verificar(double n)
+        {
+            return nota >= n;
+        }
+        //MÉTODO - Ação
+    }
+}
---
+++ POO-Aula1_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using POO_Aula1_2;
+
+double nCorte;
+Console.Write("Nota de corte: ");
+Double.TryParse(Console.ReadLine(), out nCorte);
+
+Console.Write("Quantidade de alunos: ");
+List<Aluno> alunos = new List<Aluno>(
+    Int32.Parse(Console.ReadLine())
+);
+
+for(int i = 0; i < alunos.Capacity; i++)
+{
+    Aluno a = new Aluno();
+    Console.Write($"Nome do aluno {i + 1}: ");
+    a.Nome = Console.ReadLine();
+    Console.Write($"Nota de {a.Nome}: ");
+    Double.TryParse(Console.ReadLine(), out a.nota);
+
+    alunos.Add(a);
+}
+
+List<Aluno> aprovados = new List<Aluno>(alunos.Capacity);
+foreach(Aluno a in alunos)
+    if(a.Verificar(nCorte))
+        aprovados.Add(a);
+
+Console.WriteLine("Quantidade de alunos autorizados: " +
+    aprovados.Count);
+
+foreach (Aluno a in aprovados)
+    Console.WriteLine(a.Nome);
